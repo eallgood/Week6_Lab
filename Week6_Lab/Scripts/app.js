271bce3d2cf0ba0e4adb7821578a84e9f599ac9a
eallgood <32801558+eallgood@users.noreply.github.com>
2018-02-24 13:59:00 -0800

    Initial commit

---
+++ Week6_Lab/Scripts/app.js
@@ -0,0 +1,9 @@
+﻿function summary() {
+
+    var firstName = document.getElementById("FirstName").value;
+    var lastName = document.getElementById("LastName").value;
+    var age = document.getElementById("Age").value;
+
+    var summary = firstName + " " + lastName + " is " + age + " years old."
+    document.getElementById("summary").innerText = summary;
+}
